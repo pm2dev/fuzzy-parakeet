528419e3dcac57131fa2e16caecbfb2cd4b8f865
Rob Garcia <29167041+pm2dev@users.noreply.github.com>
2024-02-01 19:44:03 -0600

    refactored the campsitecard.js by destructuring the campsite component

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,13 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
     return (
         <Card>
-            <CardImg 
-                width={'100%'}
-                src={props.campsite.image}
-                alt={props.campsite.name}
-            />
+            <CardImg width={'100%'} src={image} alt={name} />
             <CardImgOverlay>
                 <CardTitle>
-                    {props.campsite.name}
+                    {name}
                 </CardTitle>
             </CardImgOverlay>
         </Card>
