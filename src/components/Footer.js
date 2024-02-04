d49d0e6b7f686ac4be031d89bc123fb6abb5b205
Rob Garcia <29167041+pm2dev@users.noreply.github.com>
2024-02-03 22:21:25 -0600

    added router elements to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -7,7 +8,21 @@ const Footer = () => {
                 <Row>
                     {/* Column 1 - site navigation */}
                     <Col xs={{ size: 4, offset: 1}} sm='2'>
-                        <h5>This will be site navigation!</h5>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>  
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>  
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>  
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>  
+                            </li>
+                        </ul>
                     </Col>
 
                     {/* // column 2 - social media links and icons */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,61 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from '../app/assets/img/logo.png';
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import NucampLogo from "../app/assets/img/logo.png";
 
 const Header = () => {
-    return (
-        <Navbar dark color='primary' sticky='top' expand='md'> 
-        <NavbarBrand href='/'>
-          <img src={NucampLogo} alt='nucamp logo' />
-        </NavbarBrand>
-      </Navbar>
-    );
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <Navbar dark color="primary" sticky="top" expand="md">
+      <NavbarBrand className='ms-5' href="/">
+        <img className='float-start' src={NucampLogo} alt='nucamp logo' />
+        <h1 className='mt-1'>NuCamp</h1>
+      </NavbarBrand>
+
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+
+        <Nav className="ms-auto" navbar>
+          {/* NavLink1 - Home */}
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              <i className="fa fa-home fa-lg" /> Home
+            </NavLink>
+          </NavItem>
+
+          {/* NavLink2 - directory */}
+          <NavItem>
+            <NavLink className="nav-link" to="/directory">
+              <i className="fa fa-list fa-lg" /> Directory
+            </NavLink>
+          </NavItem>
+
+          {/* NavLink3 - about */}
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              <i className="fa fa-info fa-lg" /> About
+            </NavLink>
+          </NavItem>
+
+          {/* NavLink4 - contact */}
+          <NavItem>
+            <NavLink className="nav-link" to="/contact">
+              <i className="fa fa-address-card fa-lg" /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+      
+    </Navbar>
+  );
+};
 
 export default Header;
